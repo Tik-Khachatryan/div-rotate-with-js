4f3b398956dce1617e1518113fc93b0e623f608e
Tiko <tikxach20@gmail.com>
2019-12-19 10:57:53 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// let rotate = document.getElementById("rotate");
+//
+// setInterval(function rot(n){
+//
+//     if( n===1){
+//         console.log(n)
+//     }else {
+//         rotate.style.animation = "anim 3s infinite";
+//         return n * rot(n - 1)
+//     }
+// });
